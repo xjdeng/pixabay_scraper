be650db8701f6d2128dc3a47634c341acc61cff3
Xiaojian "JJ" Deng <xjd001@gmail.com>
2021-08-30 10:08:41 -0700

    Update searcher.py

--- searcher.py
+++ searcher.py
@@ -2,19 +2,29 @@ try:
     from EasyWebdriver import Chrome
 except ImportError:
     from selenium.webdriver import Chrome
+    
+import time
 
 class Search:
     
-    def __init__(self, terms, browser = None, safesearch = True):
+    def __init__(self, terms, browser = None, imgtype = "photos", safesearch = True, delay = 0.5):
         if not browser:
             browser = Chrome()
-        self.baseurl = "https://pixabay.com/photos/search/{}/?&order=latest".format(terms)
+        self.baseurl = "https://pixabay.com/{}/search/{}/?&order=latest".format(imgtype, terms)
+        self.imgtype = imgtype
+        self.delay = delay
         browser.get(self.baseurl)
         self.pages = int(browser.find_element_by_css_selector("span[class='total--2-kq8 hideMd--482UI']").\
                          get_attribute("innerHTML").replace("/ ",""))
         if safesearch:
             browser.find_element_by_css_selector("input[type='checkbox']").click()
         self.browser = browser
+        
+    def delay(self):
+        time.sleep(self.delay())
+        
+    def get_image_links(self):
+        return self.browser.find_elements_by_css_selector("a[class='link--h3bPW']")
     
     def go_page(self, page):
         newurl = "{}&pagi={}&".format(self.baseurl, page)
